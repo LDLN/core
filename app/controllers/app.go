9c129f07079decc8d4c3de1562d902f43d6894ed
Nick Ihm <nickihm@laptop.local>
2014-06-17 23:30:01 -0400

    basestation phase 2 websocket

---
+++ app/controllers/app.go
@@ -0,0 +1,68 @@
+package controllers
+
+import (
+	"code.google.com/p/go.net/websocket"
+	"github.com/revel/revel"
+	"basestation/app/chatroom"
+)
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	return c.Render()
+}
+
+func (c App) RoomSocket(user string, ws *websocket.Conn) revel.Result {
+	// Join the room.
+	subscription := chatroom.Subscribe()
+	defer subscription.Cancel()
+
+	chatroom.Join(user)
+	defer chatroom.Leave(user)
+
+	// Send down the archive.
+	for _, event := range subscription.Archive {
+		if websocket.JSON.Send(ws, &event) != nil {
+			// They disconnected
+			return nil
+		}
+	}
+
+	// In order to select between websocket messages and subscription events, we
+	// need to stuff websocket events into a channel.
+	newMessages := make(chan string)
+	go func() {
+		var msg string
+		for {
+			err := websocket.Message.Receive(ws, &msg)
+			if err != nil {
+				close(newMessages)
+				return
+			}
+			newMessages <- msg
+		}
+	}()
+
+	// Now listen for new events from either the websocket or the chatroom.
+	for {
+		select {
+		case event := <-subscription.New:
+			if websocket.JSON.Send(ws, &event) != nil {
+				// They disconnected.
+				return nil
+			}
+		case msg, ok := <-newMessages:
+			// If the channel is closed, they disconnected.
+			if !ok {
+				return nil
+			}
+
+			// Otherwise, say something.
+			chatroom.Say(user, msg)
+		}
+	}
+	return nil
+}
+
