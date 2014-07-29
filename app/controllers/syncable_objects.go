da59cf259eda53c8e78dbf422f2521386722c233
nick ihm <nickihm@gmail.com>
2014-07-28 21:27:22 -0400

    added syncable object type list

--- app/controllers/syncable_objects.go
+++ app/controllers/syncable_objects.go
@@ -1,9 +1,31 @@
 package controllers
 
 import (
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 	"github.com/revel/revel"
 )
 
 type SyncableObjects struct {
 	*revel.Controller
+}
+
+func (c SyncableObjects) ListDataTypes() revel.Result {
+	
+	// connect to mongodb
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	
+	// query
+	dbc := session.DB("landline").C("Schemas")
+	var results []map[string]interface{}
+	err = dbc.Find(bson.M{}).All(&results)
+	if err != nil {
+		revel.TRACE.Println(err)
+	}
+	
+	return c.Render(results)
 }
