b29c9949fae900bbdbed8abf416ed951e4fe43b7
Nick Ihm <nickihm@Nicks-MacBook-Pro.local>
2015-10-08 20:21:21 -0400

    fix conflicts

--- cryptoWrapper/main.go
+++ cryptoWrapper/main.go
@@ -1,21 +1,6 @@
 package cryptoWrapper
 
 import (
-<<<<<<< HEAD
-	"log"
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-//	"crypto/rsa"
-	"crypto/sha256"
-//	"crypto/x509"
-	"labix.org/v2/mgo"
-	"labix.org/v2/mgo/bson"
-	"strings"
-	"encoding/base64"
-	"encoding/hex"
-	"io"
-=======
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
@@ -30,17 +15,12 @@ import (
 
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
->>>>>>> a035a05af8841422ffcb01d7ef52295a93e448b0
 )
 
 const salt = "Yp2iD6PcTwB6upati0bPw314GrFWhUy90BIvbJTj5ETbbE8CoViDDGsJS6YHMOBq4VlwW3V00GWUMbbV"
 
 func Encrypt(value, username, password string) string {
-<<<<<<< HEAD
-	
-=======
 
->>>>>>> a035a05af8841422ffcb01d7ef52295a93e448b0
 	// hashed_password
 	hashed_password := hashPassword(username, password)
 
@@ -55,17 +35,10 @@ func Encrypt(value, username, password string) string {
 	dbu := session.DB("landline").C("Users")
 	var result map[string]string
 	err = dbu.Find(bson.M{"username": username, "hashed_password": hashed_password}).One(&result)
-<<<<<<< HEAD
-	if(err != nil) {
-		return ""
-	}
-	
-=======
 	if err != nil {
 		return ""
 	}
 
->>>>>>> a035a05af8841422ffcb01d7ef52295a93e448b0
 	// decrypt kek
 	ps := []string{password, username, salt}
 	key := []byte(string([]rune(strings.Join(ps, "-"))[0:32]))
@@ -75,16 +48,6 @@ func Encrypt(value, username, password string) string {
 	}
 	dek := string(decrypt(key, bdec))
 
-<<<<<<< HEAD
-//	// decrypt rsa private
-//	privenc, err := hex.DecodeString(result["encrypted_rsa_private"])
-//	if err != nil {
-//		log.Printf(err)
-//	}
-//	priva := decrypt(key, privenc)
-//	priv, err := x509.ParsePKCS1PrivateKey(priva)
-		
-=======
 	//	// decrypt rsa private
 	//	privenc, err := hex.DecodeString(result["encrypted_rsa_private"])
 	//	if err != nil {
@@ -93,7 +56,6 @@ func Encrypt(value, username, password string) string {
 	//	priva := decrypt(key, privenc)
 	//	priv, err := x509.ParsePKCS1PrivateKey(priva)
 
->>>>>>> a035a05af8841422ffcb01d7ef52295a93e448b0
 	return hex.EncodeToString(encrypt([]byte(dek), []byte(value)))
 }
 
@@ -110,29 +72,15 @@ func hashPassword(username, password string) string {
 	return hashed_password
 }
 
-<<<<<<< HEAD
-func encrypt(key, text []byte) []byte {
-=======
 //encrypt the text with the key provided
 //returns a byte array
 //For reference: http://crypto.stackexchange.com/questions/2476/cipher-feedback-mode
 func encrypt(key, text []byte) []byte {
 	//Get a block using the AES and key to be run later
->>>>>>> a035a05af8841422ffcb01d7ef52295a93e448b0
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		panic(err)
 	}
-<<<<<<< HEAD
-	b := encodeBase64(text)
-	ciphertext := make([]byte, aes.BlockSize+len(b))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		panic(err)
-	}
-	cfb := cipher.NewCFBEncrypter(block, iv)
-	cfb.XORKeyStream(ciphertext[aes.BlockSize:], []byte(b))
-=======
 
 	//Encode the text as base64
 	b := encodeBase64(text)
@@ -161,28 +109,15 @@ func encrypt(key, text []byte) []byte {
 
 	//According to https://play.golang.org/p/83dbz8bvYw
 	//We should be running crypto/hmac on this before agreeing it's encrypted
->>>>>>> a035a05af8841422ffcb01d7ef52295a93e448b0
 	return ciphertext
 }
 
 func decrypt(key, text []byte) []byte {
-<<<<<<< HEAD
-=======
 	//Get a block using the AES and key to be run later
->>>>>>> a035a05af8841422ffcb01d7ef52295a93e448b0
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		panic(err)
 	}
-<<<<<<< HEAD
-	if len(text) < aes.BlockSize {
-		panic("ciphertext too short")
-	}
-	iv := text[:aes.BlockSize]
-	text = text[aes.BlockSize:]
-	cfb := cipher.NewCFBDecrypter(block, iv)
-	cfb.XORKeyStream(text, text)
-=======
 
 	//Error out if the length of the byte array to be decrypted is less than a block size
 	//This might have to be multiples of the block size if we do padding.
@@ -206,7 +141,6 @@ func decrypt(key, text []byte) []byte {
 	cfb.XORKeyStream(text, text)
 
 	//return the base64 encoded string
->>>>>>> a035a05af8841422ffcb01d7ef52295a93e448b0
 	return decodeBase64(string(text))
 }
 
@@ -220,8 +154,4 @@ func decodeBase64(s string) []byte {
 		panic(err)
 	}
 	return data
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> a035a05af8841422ffcb01d7ef52295a93e448b0
